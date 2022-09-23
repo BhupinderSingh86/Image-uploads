9adbb2b8052a82d0d4f1892a4db415c4a207814c
m4tchka <91279022+m4tchka@users.noreply.github.com>
2022-09-23 14:47:25 +0100

    working gallery

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -2,32 +2,90 @@ import React, { useState } from "react";
 import UploadButton from "../UploadButton/UploadButton";
 import "./Gallery.css";
 export default function Gallery() {
-    const [gallery, setGallery] = useState([
-        "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png",
-        "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png",
-        "https://pic.onlinewebfonts.com/svg/img_206976.png",
-        "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png",
-        "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png",
-        "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png",
-    ]);
+    const [gallery1, setGallery1] = useState(
+        "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png"
+    );
+    const [gallery2, setGallery2] = useState(
+        "https://pic.onlinewebfonts.com/svg/img_206976.png"
+    );
+    const [gallery3, setGallery3] = useState(
+        "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png"
+    );
+    const [gallery4, setGallery4] = useState(
+        "https://pic.onlinewebfonts.com/svg/img_206976.png"
+    );
+    const [gallery5, setGallery5] = useState(
+        "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png"
+    );
+    const [gallery6, setGallery6] = useState(
+        "https://pic.onlinewebfonts.com/svg/img_206976.png"
+    );
     // const [galleryItem, setGalleryItem] = useState();
-
+    // "https://pic.onlinewebfonts.com/svg/img_206976.png"
     //localStorage.setItem("gallery", data);
 
     return (
         <div className="Gallery">
-            {gallery.map((galleryEntry) => {
-                return (
-                    <div className="gallery-entry">
-                        <img
-                            className="gallery-image"
-                            src={galleryEntry}
-                            alt="galleryItem"
-                        ></img>
-                    </div>
-                );
-            })}
-            <UploadButton input={setGallery} />
+            <div className="gallery-entry">
+                <img
+                    className="gallery-image"
+                    src={gallery1}
+                    alt="galleryItem"
+                ></img>
+                <div className="upload-button-container">
+                    <UploadButton input={setGallery1} />
+                </div>
+            </div>
+            <div className="gallery-entry">
+                <img
+                    className="gallery-image"
+                    src={gallery2}
+                    alt="galleryItem"
+                ></img>
+                <div className="upload-button-container">
+                    <UploadButton input={setGallery2} />
+                </div>
+            </div>
+            <div className="gallery-entry">
+                <img
+                    className="gallery-image"
+                    src={gallery3}
+                    alt="galleryItem"
+                ></img>
+                <div className="upload-button-container">
+                    <UploadButton input={setGallery3} />
+                </div>
+            </div>
+            <div className="gallery-entry">
+                <img
+                    className="gallery-image"
+                    src={gallery4}
+                    alt="galleryItem"
+                ></img>
+                <div className="upload-button-container">
+                    <UploadButton input={setGallery4} />
+                </div>
+            </div>
+            <div className="gallery-entry">
+                <img
+                    className="gallery-image"
+                    src={gallery5}
+                    alt="galleryItem"
+                ></img>
+                <div className="upload-button-container">
+                    <UploadButton input={setGallery5} />
+                </div>
+            </div>
+            <div className="gallery-entry">
+                <img
+                    className="gallery-image"
+                    src={gallery6}
+                    alt="galleryItem"
+                ></img>
+                <div className="upload-button-container">
+                    <UploadButton input={setGallery6} />
+                </div>
+            </div>
         </div>
     );
 }
